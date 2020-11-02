94c25c8eea8e2f58339bbe12e3fd44618aa1abd1
Felipe <fdaniel_93@hotmail.com>
2020-11-02 00:14:27 -0600

    subiendo archivos

---
+++ js/script2.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('monster'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data2.json' 
+
+});
